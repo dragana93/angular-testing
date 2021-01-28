51e3b4afb4735ac7ba97c8135edd49b1433fea1d
draganasavanovic <dragana.savanovic@wayseven.com>
2021-01-28 14:03:39 +0100

    angular testing

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,41 @@
+import { TestBed } from "@angular/core/testing";
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
+describe("CalculatorService", () => {
+  let calculator: CalculatorService;
+  let loggerSpy: any;
+
+  beforeEach(() => {
+    console.log("Calling beforeEach");
+    loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy },
+      ],
+    });
+    calculator = TestBed.inject(CalculatorService);
+  });
+
+  it("should add two numbers", () => {
+    console.log("add test");
+    const result = calculator.add(2, 2);
+    expect(result).toBe(4);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
+
+  it("should subtract two numbers", () => {
+    console.log("subtract test");
+    const result = calculator.subtract(5, 2);
+    expect(result).toBe(3);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
+
+  it("should multiplied two numbers", () => {
+    console.log("multiplication test");
+    const result = calculator.multiplication(2, 2);
+    expect(result).toBe(4);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
+});
